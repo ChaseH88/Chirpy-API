3100b02767d6e119764487a441d5056257bf0c76
Chase Harrison <ChaseH88@gmail.com>
2024-02-18 12:40:40 -0600

    enhance create user mutation

--- src/mutation/create-user.ts
+++ src/mutation/create-user.ts
@@ -1,7 +1,22 @@
 import { UserModel } from '../models/user';
 
-export const createUser = async (_, args) => {
-  const user = new UserModel(args.data);
-  await user.save();
-  return user;
+interface CreateUserInput {
+  data: {
+    username: string;
+    password: string;
+    email: string;
+  };
+}
+
+export const createUser = async (_, args: CreateUserInput) => {
+  const existingUser = await UserModel.findOne({
+    $or: [{ email: args.data.email }, { username: args.data.username }],
+  });
+
+  if (existingUser) {
+    throw new Error('User already exists');
+  }
+
+  const user = await UserModel.create(args.data);
+  return user.populate('posts');
 };
