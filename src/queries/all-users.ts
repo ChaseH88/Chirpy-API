3190196faf4c60449b50eb0a5ea0a839454203ef
Chase Harrison <ChaseH88@gmail.com>
2024-02-18 13:04:31 -0600

    remove password from gql query

--- src/queries/all-users.ts
+++ src/queries/all-users.ts
@@ -1,7 +1,9 @@
 import { UserModel } from '../models/user';
 
 export const allUsers = async () =>
-  await UserModel.find().populate({
-    path: 'posts',
-    model: 'PostModel',
-  });
+  await UserModel.find()
+    .populate({
+      path: 'posts',
+      model: 'PostModel',
+    })
+    .select('-password');
