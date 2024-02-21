dac2fffa61799b691e230cc31371ba0d6a86644b
Chase Harrison <ChaseH88@gmail.com>
2024-02-21 17:28:15 -0600

    remove temp error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { MaskError, createSchema, createYoga } from 'graphql-yoga';
-import { GraphQLError } from 'graphql';
-import { Database } from './classes/Database';
-import schema from './schema';
-import { Query } from './queries';
-import { Mutation } from './mutation';
+import { MaskError, createSchema, createYoga } from "graphql-yoga";
+import { GraphQLError } from "graphql";
+import { Database } from "./classes/Database";
+import schema from "./schema";
+import { Query } from "./queries";
+import { Mutation } from "./mutation";
 
 new Database().start();
 
@@ -15,9 +15,6 @@ const yoga = createYoga({
       Mutation,
     },
   }),
-  maskedErrors: {
-    errorMessage: 'An error occurred',
-  },
 });
 
 const init = () => {
