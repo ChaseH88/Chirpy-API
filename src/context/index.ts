646ec8f7cea2fc9c8a55efff1ffa9b72ac901774
Chase Harrison <ChaseH88@gmail.com>
2024-02-25 06:55:55 -0600

    update context and edit user to handle auth

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,9 +1,24 @@
-import { UserModel } from "../models/user";
-import { decodeToken } from "../utilities/json-web-token";
+import { UserModel, UserModelInterface } from '../models/user';
+import { decodeToken } from '../utilities/json-web-token';
+
+export interface Context {
+  currentUser: Pick<
+    UserModelInterface,
+    | 'id'
+    | 'email'
+    | 'username'
+    | 'firstName'
+    | 'lastName'
+    | 'bio'
+    | 'photo'
+    | 'createdAt'
+    | 'updatedAt'
+  >;
+}
 
 export const apiContext = async (req: any) => {
   const userId =
-    decodeToken(req.request.headers.get("authorization"))?.userId || null;
+    decodeToken(req.request.headers.get('authorization'))?.userId || null;
   const user = userId ? await UserModel.findById(userId) : null;
   return {
     ...req,
--- src/mutation/edit-user.ts
+++ src/mutation/edit-user.ts
@@ -1,22 +1,28 @@
-import { PostModel } from "../models/post";
-import { UserModel, UserModelInterface } from "../models/user";
+import { Context } from '../context';
+import { PostModel } from '../models/post';
+import { UserModel, UserModelInterface } from '../models/user';
 
 interface EditUserInput {
-  id: Pick<UserModelInterface, "id">;
+  id: Pick<UserModelInterface, 'id'>;
   data: Pick<
     UserModelInterface,
-    "email" | "username" | "firstName" | "lastName" | "bio" | "photo"
+    'email' | 'username' | 'firstName' | 'lastName' | 'bio' | 'photo'
   >;
 }
 
-export const editUser = async (_, args: EditUserInput) => {
-  const existingUser = await UserModel.findById(args.id);
+export const editUser = async (_, args: EditUserInput, ctx: Context) => {
+  const currentUser = await UserModel.findById(ctx.currentUser.id);
+  const userToEdit = await UserModel.findById(args.id);
 
-  if (!existingUser) {
-    throw new Error("User not found");
+  if (!userToEdit) {
+    throw new Error('User not found');
+  }
+
+  if (currentUser?.id !== userToEdit.id) {
+    throw new Error('You are not authorized to edit this user');
   }
 
   return await UserModel.findByIdAndUpdate(args.id, args.data, {
     new: true,
-  }).populate("posts");
+  }).populate('posts');
 };
