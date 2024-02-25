2a4c4e24c321064f170b94998e293db711500113
Chase Harrison <ChaseH88@gmail.com>
2024-02-25 06:59:07 -0600

    add auth to delete user

--- src/mutation/delete-user.ts
+++ src/mutation/delete-user.ts
@@ -1,3 +1,4 @@
+import { Context } from '../context';
 import { PostModel } from '../models/post';
 import { UserModel } from '../models/user';
 
@@ -5,11 +6,15 @@ interface DeleteUserInput {
   id: string;
 }
 
-export const deleteUser = async (_, { id }: DeleteUserInput) => {
-  const existingUser = await UserModel.findById(id);
+export const deleteUser = async (_, { id }: DeleteUserInput, ctx: Context) => {
+  const userToDelete = await UserModel.findById(id);
 
-  if (!existingUser) {
+  if (!userToDelete) {
     throw new Error('User not found');
+  }
+
+  if (ctx?.currentUser?.id !== userToDelete.id) {
+    throw new Error('You are not authorized to delete this user');
   }
 
   await UserModel.findByIdAndDelete(id);
