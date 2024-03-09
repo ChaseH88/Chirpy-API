2ce8ac7d26afeb7675daab4a8c1e8da69c822c17
Chase Harrison <ChaseH88@gmail.com>
2024-03-09 12:01:11 -0600

    authentication on message sent subscription

--- src/subscription/message-sent.ts
+++ src/subscription/message-sent.ts
@@ -1,5 +1,8 @@
+import { isAuthenticated } from "../utilities/is-authenticated";
+
 export const messageSent = {
-  subscribe: async (_, __, { pubSub, currentUser }) =>
-    pubSub.subscribe(`message:${currentUser.id}`),
+  subscribe: isAuthenticated(async (_, __, { pubSub, currentUser }) =>
+    pubSub.subscribe(`message:${currentUser.id}`)
+  ),
   resolve: async ({ message }: any) => message,
 };
