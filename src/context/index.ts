eab0a701ea97aa5c3b7d845acf3154ed7a2cc79b
Chase Harrison <ChaseH88@gmail.com>
2024-03-10 09:05:18 -0500

    update context to handle bearer string

--- src/context/index.ts
+++ src/context/index.ts
@@ -21,8 +21,9 @@ export interface Context {
 }
 
 export const apiContext = async (req: any) => {
-  const userId =
-    decodeToken(req.request.headers.get("authorization"))?.userId || null;
+  const authHeader =
+    req.request.headers.get("authorization")?.split(" ")[1] || null;
+  const userId = decodeToken(authHeader)?.userId || null;
   const user = userId ? await UserModel.findById(userId) : null;
   return {
     ...req,
