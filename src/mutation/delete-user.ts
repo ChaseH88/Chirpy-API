5020d5b85f84162b1761922ee521dbd168b8d9a3
Chase Harrison <ChaseH88@gmail.com>
2024-03-16 22:30:51 -0500

    update delete user

--- src/mutation/delete-user.ts
+++ src/mutation/delete-user.ts
@@ -1,8 +1,8 @@
 import { Context } from "../context";
-import { PostModel } from "../models/post";
-import { UserModel } from "../models/user";
 import { GraphQLError } from "graphql";
 import { isAuthenticated } from "../utilities/is-authenticated";
+import { PostModel } from "../models/post";
+import { UserModel } from "../models/user";
 
 interface DeleteUserInput {
   id: string;
@@ -10,19 +10,45 @@ interface DeleteUserInput {
 
 export const deleteUser = isAuthenticated(
   async (_, { id }: DeleteUserInput, ctx: Context) => {
-    const userToDelete = await UserModel.findById(id);
+    const session = await UserModel.startSession();
+    session.startTransaction();
 
-    if (!userToDelete) {
-      throw new GraphQLError("User not found");
+    try {
+      const userToDelete = await UserModel.findById(id).session(session);
+      if (!userToDelete) {
+        throw new GraphQLError("User not found");
+      }
+
+      if (ctx?.currentUser?.id !== userToDelete.id) {
+        throw new GraphQLError("You are not authorized to delete this user");
+      }
+
+      await UserModel.findByIdAndDelete(id).session(session);
+      await PostModel.deleteMany({ postedBy: id }).session(session);
+
+      const updateConditions = [
+        { following: id },
+        { followers: id },
+        { blocked: id },
+        { likes: id },
+        { dislikes: id },
+      ];
+      for (let condition of updateConditions) {
+        await UserModel.updateMany(condition, { $pull: condition }).session(
+          session
+        );
+        await PostModel.updateMany(condition, { $pull: condition }).session(
+          session
+        );
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+      return "User deleted successfully";
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+      throw error;
     }
-
-    if (ctx?.currentUser?.id !== userToDelete.id) {
-      throw new GraphQLError("You are not authorized to delete this user");
-    }
-
-    await UserModel.findByIdAndDelete(id);
-    await PostModel.deleteMany({ postedBy: id });
-
-    return "User deleted successfully";
   }
 );
